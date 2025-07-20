19cbc8772df0a6ae693d5694ff45e3f7511eccc9
Al Mahady <mahadypanchagarh@gmail.com>
2025-07-20 21:58:12 +0600

    Create 2123D.cpp

---
+++ 2123D.cpp
@@ -0,0 +1,27 @@
+//Be afraid , Here lives a Monster//
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl endl
+
+void solve(){
+    ll n,k;
+    cin>>n>>k;
+
+    string s;
+    cin>>s;
+
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
