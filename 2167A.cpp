7a6067d6ea0e5499b749a83623bb0575b7d6b247
Al Mahady <mahadypanchagarh@gmail.com>
2025-10-30 14:31:06 +0600

    New files added

---
+++ 2167A.cpp
@@ -0,0 +1,27 @@
+//Be afraid , Here lives a Monster//
+
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl endl
+
+void solve(){
+    ll a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    if(a==b && b==c && c==d && d==a){
+        cout<<"YES"<<nl;
+    }
+    else cout<<"NO"<<nl;
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
